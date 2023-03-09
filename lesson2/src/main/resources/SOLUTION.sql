1698905c705e1e8f67e6b4f350a3953b3616e23e
Staluks <janchukssss@inbox.lv>
2023-03-09 16:39:08 +0200

    sixth try 2tasks

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,28 +1,28 @@
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('John', 1990-07-31, 1);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Chris', 1990-07-01, 1);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Carl', 1990-07-02, 1);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Oliver', 1991-07-01, 2);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('James', 1991-07-02, 2);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Lucas', 1991-07-03, 2);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Henry', 1991-07-04, 2);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Jacob', 1992-07-01, 3);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Logan', 1992-07-02, 3);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Agris', 1993-07-01, 4);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Ritvars', 1993-07-02, 4);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Janis', 1994-07-01, 5);
-INSERT INTO students(name, birthday, groupnumber)
+INSERT INTO student(name, birthday, groupnumber)
 VALUES('Artis', 1994-07-02, 5);
 
 INSERT INTO subject(name, description, grade)
